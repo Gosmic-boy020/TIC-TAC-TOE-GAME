52f2acb8cc7ba637fceeaf0b2b09bd15943c5327
Gosmic-boy020 <163012450+Gosmic-boy020@users.noreply.github.com>
2025-06-18 16:56:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 import { toss } from "./computer.js";
 
 import { cellsDisplay } from "./twoPlayersMod.js";
+import { messageToPage } from "./publicFunctions.js";
 
 
 function selectOptionsDisplay(){
   const html=`
-  <select name="" class="sel-opti">
-    <option  selected>chosse option</option>
-    <option  class="player-mod" data-mod="0" >🙎‍♂️vs🙎</option>
-    <option class="player-mod" data-mod="1">🙎‍♂️ vs B🤖T</option> 
-  </select> `;
+  <buttton  class="player-mod" data-mod="0" onclick="dis()">🙎‍♂️vs🙎</buttton>
+    <buttton class="player-mod" data-mod="1">🙎‍♂️ vs B🤖T</buttton>`;
 
-  document.querySelector('.select-options').innerHTML=html;
+  document.querySelector('.sel-opi').innerHTML=html;
 
+//EventListener Here////////////////////////////////
   document.querySelectorAll('.player-mod').forEach((item)=>{
     item.addEventListener('click',()=>{
+    
+     const ele=document.querySelector('.sel-opi');
+      ele.classList.remove("options-show");
+  
+      document.querySelectorAll('.player-mod').forEach((item)=>{
+        item.classList.remove("play-mod-show");
+      });
+      
       const modvalue=item.dataset.mod;
       const modNam=item.innerHTML;
       if(modvalue=='0'){
@@ -27,6 +34,18 @@ function selectOptionsDisplay(){
     });
     
   });
+document.querySelector('.select-options').addEventListener('click',()=>{
+  messageToPage('game-pad','');
+      messageToPage('msg','');
+      messageToPage('toss-area','')
+  const ele=document.querySelector('.sel-opi');
+  ele.classList.toggle("options-show");
+  
+  document.querySelectorAll('.player-mod').forEach((item)=>{
+    item.classList.toggle("play-mod-show");
+  });
+});
+
 
 }
 
